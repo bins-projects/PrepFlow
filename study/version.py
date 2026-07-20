383924334975cc988e53a892127d43c3d93aedc2
PrepFlow <bins-projects@users.noreply.github.com>
2026-07-20 19:38:36 -0400

    refactor: remove legacy desktop application

--- study/version.py
+++
@@ -1,8 +0,0 @@
-"""PrepFlow application version and release information."""
-
-APP_VERSION = "1.1.0"
-
-RELEASES_URL = "https://github.com/bins-projects/PrepFlow/releases/latest"
-LATEST_RELEASE_API_URL = (
-    "https://api.github.com/repos/bins-projects/PrepFlow/releases/latest"
-)
