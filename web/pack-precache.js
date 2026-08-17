91114de1064b86822beca186bb5831decf8f30ac
bins-projects <charliekeila@gmail.com>
2026-08-17 10:19:53 -0400

    Add touch-safe Start Quiz activation (#18)
    * Add touch-safe Start Quiz activation
    
    * Bump PWA cache for touch-safe Start Quiz

--- web/pack-precache.js
+++ web/pack-precache.js
@@ -1,1 +1,1 @@
-self.PREPFLOW_PACK_PRECACHE = {"version": "start-quiz-hit-area-20260817-1", "urls": ["../packs/fundamentals.prepflow.json", "../packs/medical_surgical.prepflow.json", "../packs/pediatrics.prepflow.json", "../packs/pharmacy.prepflow.json"]};
+self.PREPFLOW_PACK_PRECACHE = {"version": "touch-safe-start-quiz-20260817-1", "urls": ["../packs/fundamentals.prepflow.json", "../packs/medical_surgical.prepflow.json", "../packs/pediatrics.prepflow.json", "../packs/pharmacy.prepflow.json"]};
